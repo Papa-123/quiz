f27a094bebde02161696c312be893e87db94360b
Papa-123 <109425326+Papa-123@users.noreply.github.com>
2022-11-16 10:22:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+import time
+time.sleep(1)
+print("Welcome to a very interesting quiz")
+
+chances = 1
+print("You will have", chances, "chance to answer correctly.\nPlease speak to answer\n")
+time.sleep(2)
+score = 0
+question_1 = print(" 1) What type of weapon does Nobara use to exorcise curses in Jujutsu Kaisen?\n(a) Hammer \n(b) Sword \n(c) Belt \n(d) Salt \n\n")
+answer_1 = "a"
+
+for i in range(chances):
+    from speech_1 import call
+    answer = call()
+    if (answer.lower()  == answer_1):
+        print("Correct!\n")
+        score = score +1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_1, "\n\n")
+
+time.sleep(2)
+
+question_2 = print(" 2) Who is the cyborg character in One Puch Man?\n(a) Saitama \n(b) Fang \n(c) Mumen Rider \n(d) Genos \n\n")
+answer_2 = "d"
+
+for i in range(chances):
+    from speech_2 import call_2
+    answer = call_2()
+    if (answer.lower()  == answer_2):
+        print("Correct!\n")
+        score = score +1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_2, "\n\n")
+
+time.sleep(2)
+
+question_3 = print(" 3) Who is Gojo fighting when he teaches Itadori about domain expansion?\n(a) Jogo \n(b) Mahito \n(c) Sugugru Geto \n(d) Vortex \n\n")
+answer_3 = "a"
+
+for i in range(chances):
+    from speech_3 import call_3
+    answer = call_3()
+    if (answer.lower()  == answer_3):
+        print("Correct!\n")
+        score = score +1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_3, "\n\n")
+
+time.sleep(2)
+
+question_4 = print(" 4)Where did Itadori and Nanami investigate the massacre by Mahito?\n(a) Movie Theater \n(b) Mall \n(c) Grocery Store \n(d) Pub \n\n")
+answer_4 = "a"
+
+for i in range(chances):
+    from speech_4 import call_4
+    answer = call_4()
+    if (answer.lower()  == answer_4):
+        print("Correct!\n")
+        score = score +1
+        break
+    else:
+        print("Incorrect!\n")
+        time.sleep(0.5)
+        print("The correct answer is", answer_4, "\n\n")
+
+time.sleep(2)
+
+while score>=2:
+    print("Well Done! Your score was",score)
+    break
+while score<2:
+    print("Better Luck next time! Your score was", score)
+    break
